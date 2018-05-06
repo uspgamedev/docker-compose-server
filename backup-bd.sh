c6173a4c9a6bd5ae30b76a5fbdbd726185e552fa
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2018-05-06 20:06:41 -0300

    Improve backup

--- backup-bd.sh
+++ backup-bd.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+# wordpress
+tar czf /var/docker-www/backups/wordpress/wordpress.tar.gz /var/docker-www/data/wordpress/
+
 # database
-docker run --rm -v /var/docker-www/config/mysqldump-my.cnf:/root/.my.cnf:ro --network uspgamedev_default mysql mysqldump --all-databases -h db > /var/docker-www/backups/database/dump_mysql_$(date +%d-%m-%Y"_"%H_%M_%S).sql
-su postgres -c pg_dumpall > /var/docker-www/backups/database/dump_postgre_$(date +%d-%m-%Y"_"%H_%M_%S).sql
-rsync -av /var/docker-www/backups/database/ uspgamedev@shell.ime.usp.br:private/backups/database/
+docker run --rm -v /var/docker-www/config/mysqldump-my.cnf:/root/.my.cnf:ro --network uspgamedev_default mysql mysqldump --all-databases -h db \
+    | gzip --rsyncable > /var/docker-www/backups/database/dump_mysql_$(date +%d-%m-%Y"_"%H_%M_%S).sql.gz
+su postgres -c pg_dumpall \
+    | gzip --rsyncable > /var/docker-www/backups/database/dump_postgre_$(date +%d-%m-%Y"_"%H_%M_%S).sql
+rsync -av --delete /var/docker-www/backups/database/ uspgamedev@shell.ime.usp.br:private/backups/database/
 
 # discourse backups
-rsync -av /var/docker-www/data/discourse/standalone/backups/default/ uspgamedev@shell.ime.usp.br:private/backups/discourse
+rsync -av --delete /var/docker-www/data/discourse/standalone/backups/default/ uspgamedev@shell.ime.usp.br:private/backups/discourse
