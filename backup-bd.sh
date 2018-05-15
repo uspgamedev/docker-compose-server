7c605d1c2ce46a3c00d28c9af457e744482d92a9
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2018-05-15 02:07:26 -0300

    Fix backup

--- backup-bd.sh
+++ backup-bd.sh
@@ -3,5 +3,5 @@
 docker run --rm -v /var/docker-www/config/mysqldump-my.cnf:/root/.my.cnf:ro --network uspgamedev_default mysql mysqldump --all-databases -h db \
     | gzip --rsyncable > /var/docker-www/backups/database/dump_mysql_$(date +%d-%m-%Y"_"%H_%M_%S).sql.gz
 su postgres -c pg_dumpall \
-    | gzip --rsyncable > /var/docker-www/backups/database/dump_postgre_$(date +%d-%m-%Y"_"%H_%M_%S).sql
+    | gzip --rsyncable > /var/docker-www/backups/database/dump_postgre_$(date +%d-%m-%Y"_"%H_%M_%S).sql.gz
 
