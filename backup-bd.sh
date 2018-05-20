8a790b2e83b67ca990b00ed764b586204db3957b
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2018-05-20 12:02:13 -0300

    Use correct mysql version for dump

--- backup-bd.sh
+++ backup-bd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker run --rm -v /var/docker-www/config/mysqldump-my.cnf:/root/.my.cnf:ro --network uspgamedev_default mysql mysqldump --all-databases -h db \
+docker run --rm -v /var/docker-www/config/mysqldump-my.cnf:/root/.my.cnf:ro --network uspgamedev_default mysql:5.7 mysqldump --all-databases -h db \
     | gzip --rsyncable > /var/docker-www/backups/database/dump_mysql_$(date +%d-%m-%Y"_"%H_%M_%S).sql.gz
 su postgres -c pg_dumpall \
     | gzip --rsyncable > /var/docker-www/backups/database/dump_postgre_$(date +%d-%m-%Y"_"%H_%M_%S).sql.gz
