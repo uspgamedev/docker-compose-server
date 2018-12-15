cbf284bccd4486754ce1ffe4a05bd167bff4bfb8
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2018-12-15 13:03:06 -0200

    Send only latest files to remote

--- backup-and-upload.sh
+++ backup-and-upload.sh
@@ -4,10 +4,20 @@ set -e
 dir=$(dirname "$(readlink -f "$0")")
 
 bash $dir/backup-bd.sh
+
+# Send only the latest backups to the server, but keep all old copies locally
+rm -rf /var/docker-www/backups/database-latest/
+mkdir -p /var/docker-www/backups/database-latest/
+find /var/docker-www/backups/database/ -maxdepth 1 -type f -print0 \
+     | xargs -0 stat --printf "%Y\t%n\n" \
+     | sort -n \
+     | tail -n 10 \
+     | cut -f 2 \
+     | xargs cp -a -t /var/docker-www/backups/database-latest/
+
 bash $dir/backup-discourse.sh
 
-rsync -av --delete /var/docker-www/backups/database/ uspgamedev@shell.ime.usp.br:private/backups/database/
-
+rsync -av --delete /var/docker-www/backups/database-latest/ uspgamedev@shell.ime.usp.br:private/backups/database/
 rsync -av --delete /var/docker-www/backups/discourse.tar.xz uspgamedev@shell.ime.usp.br:private/backups/discourse/
 
 echo $dirname
